193da4ffa28fae1cf08c009f9a6aa5425f3c7c60
Jason William Nursalim <jwnursalim@gmail.com>
2024-02-01 00:07:02 +0700

    Recipe & Tags API

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -10,7 +10,6 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """ Django command to wait for db """
-    
     def handle(self, *args, **options):
         """ Entry point for command. """
         self.stdout.write("Waiting for database...")
@@ -22,5 +21,5 @@ class Command(BaseCommand):
             except(Psycopg2Error, OperationalError):
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
+                
         self.stdout.write(self.style.SUCCESS('Database available!'))
-
